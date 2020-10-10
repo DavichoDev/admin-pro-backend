03404cd6071e4ebdf445d373ca2d65d8f33a36a6
DavichoDev <gamers_wow@hotmail.com>
2020-10-09 23:37:35 -0500

    HerokuFix

--- database/config.js
+++ database/config.js
@@ -5,12 +5,7 @@ let dbConnection = async () => {
 
     try {
 
-        await mongoose.connect(
-            process.env.DB_CNN, 
-        {
-            useNewUrlParser: true,  
-            useUnifiedTopology: true
-        });
+        await mongoose.connect("mongodb+srv://DavichoDev:Interpol123@@cluster0.reday.mongodb.net/hospitaldb",{useNewUrlParser: true,useUnifiedTopology: true});
 
         console.log('========================>');
         console.log('----- DB en linea ------');
